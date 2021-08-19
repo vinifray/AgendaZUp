fb6fdd0075314f394642a8a8597980885e482b32
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-19 15:14:54 -0300

    Criar model para contato

---
+++ src/main/java/br/com/zup/ZupAgenda/models/Contato.java
@@ -0,0 +1,51 @@
+package br.com.zup.ZupAgenda.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "contatos")
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String telefone;
+
+    public Contato() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}

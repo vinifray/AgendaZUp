62d617f2803c37a62643a5eb78ec7c58a6450861
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-19 14:05:45 -0300

    Iniciar projeto

---
+++ src/main/java/br/com/zup/ZupAgenda/ZupAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ZupAgenda/ZupAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

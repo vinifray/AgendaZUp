639891dbe164cc78c50f2cddff61971a0a5d9f87
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-19 15:49:34 -0300

    Implementar metodo para cadastrar contato

--- src/main/java/br/com/zup/ZupAgenda/services/ContatoService.java
+++ src/main/java/br/com/zup/ZupAgenda/services/ContatoService.java
@@ -1,5 +1,6 @@
 package br.com.zup.ZupAgenda.services;
 
+import br.com.zup.ZupAgenda.models.Contato;
 import br.com.zup.ZupAgenda.repositories.ContatoRepostory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Service;
 public class ContatoService {
     @Autowired
     private ContatoRepostory contatoRepostory;
+
+    public Contato cadastrarContato(Contato contato){
+        return contatoRepostory.save(contato);
+    }
 
 }

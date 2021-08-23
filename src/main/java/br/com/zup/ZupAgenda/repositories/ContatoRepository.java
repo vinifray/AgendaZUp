a4b3e42834b13aa43e563a7561f654255c5a372c
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-23 15:02:12 -0300

    Implementa filtro por inicio de letra

--- src/main/java/br/com/zup/ZupAgenda/repositories/ContatoRepository.java
+++ src/main/java/br/com/zup/ZupAgenda/repositories/ContatoRepository.java
@@ -3,5 +3,8 @@ package br.com.zup.ZupAgenda.repositories;
 import br.com.zup.ZupAgenda.models.Contato;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContatoRepository extends CrudRepository<Contato, Integer> {
+    List<Contato> findByNomeStartsWithIgnoreCase(String letras);
 }

5b26b84035f72abf0a51927a3afd8445ebe6948c
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-24 13:50:16 -0300

    Criar rota de acesso para cadastrar endereco

---
+++ src/main/java/br/com/zup/ZupAgenda/Endereco/EnderecoController.java
@@ -0,0 +1,16 @@
+package br.com.zup.ZupAgenda.Endereco;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @PutMapping("/{idContato}")
+    public Endereco cadastrarEndereco(@RequestBody Endereco endereco, @PathVariable int idContato){
+        return enderecoService.cadastrarEndereço(idContato, endereco);
+    }
+}

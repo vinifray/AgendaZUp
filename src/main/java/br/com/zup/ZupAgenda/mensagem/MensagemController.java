a4dff5f69c228c3b5e02b7398e68ceebb1a7ec86
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-24 17:06:18 -0300

    Corrir resposta http do controller

--- src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemController.java
+++ src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemController.java
@@ -4,6 +4,7 @@ import br.com.zup.ZupAgenda.mensagem.dtos.CadastroMensagemDTO;
 import br.com.zup.ZupAgenda.mensagem.dtos.MensagemDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,6 +16,7 @@ public class MensagemController {
     private MensagemService mensagemService;
 
     @PostMapping("/{contatoId}")
+    @ResponseStatus(HttpStatus.CREATED)
     public MensagemDTO cadastrarMensagem(@RequestBody @Valid CadastroMensagemDTO mensagemDTO, @PathVariable int contatoId){
         Mensagem mensagem = mensagemDTO.converterDTOParaModel();
         mensagem = mensagemService.cadastrarMensagem(contatoId, mensagem);

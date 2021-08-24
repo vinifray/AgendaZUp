c96d28ed106493c7b8cf7c1f407bc525c9bed411
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-24 16:43:13 -0300

    Implementa interface de repositorio para mensagem

---
+++ src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.ZupAgenda.mensagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MensagemRepository extends CrudRepository<Mensagem, Integer> {
+}

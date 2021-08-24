0d8ac0b499d976b873ccaea0c138784c129f152d
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-24 13:19:49 -0300

    Criar interface de repositorio para endereco

---
+++ src/main/java/br/com/zup/ZupAgenda/Endereco/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.ZupAgenda.Endereco;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, Integer> {
+}

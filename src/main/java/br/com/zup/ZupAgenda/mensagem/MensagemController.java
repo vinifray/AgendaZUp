39267236c810877eb161d6a10c3e9436c3ab52e1
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-24 16:47:20 -0300

    Implementar metodo de cadastro de mensagem

--- src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemController.java
+++ src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemController.java
@@ -1,6 +1,7 @@
 package br.com.zup.ZupAgenda.mensagem;
 
 import br.com.zup.ZupAgenda.mensagem.dtos.CadastroMensagemDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -8,10 +9,12 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/mensagens")
 public class MensagemController {
-
+    @Autowired
+    private MensagemService mensagemService;
 
     @PostMapping("/{contatoId}")
     public Mensagem cadastrarMensagem(@RequestBody @Valid CadastroMensagemDTO mensagemDTO, @PathVariable int contatoId){
         Mensagem mensagem = mensagemDTO.converterDTOParaModel();
+        return mensagemService.cadastrarMensagem(contatoId, mensagem);
     }
 }
--- src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemService.java
+++ src/main/java/br/com/zup/ZupAgenda/mensagem/MensagemService.java
@@ -1,8 +1,11 @@
 package br.com.zup.ZupAgenda.mensagem;
 
+import br.com.zup.ZupAgenda.contato.Contato;
 import br.com.zup.ZupAgenda.contato.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class MensagemService {
@@ -15,5 +18,13 @@ public class MensagemService {
         this.contatoService = contatoService;
     }
 
+    public Mensagem cadastrarMensagem(int contatoId, Mensagem mensagem){
+        Contato contato = contatoService.buscarContatoPeloId(contatoId);
 
+        mensagem.setContato(contato);
+        mensagem.setDataDeEnvio(LocalDate.now());
+        mensagem.setVisualizado(false);
+
+        return mensagemRepository.save(mensagem);
+    }
 }

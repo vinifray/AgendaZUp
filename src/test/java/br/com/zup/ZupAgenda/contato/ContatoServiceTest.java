31a8a0eb32d8a8ede816d12a67fe94bc086542d0
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-31 14:33:05 -0300

    Adicionar teste para busca de contato pelo nome

--- src/test/java/br/com/zup/ZupAgenda/contato/ContatoServiceTest.java
+++ src/test/java/br/com/zup/ZupAgenda/contato/ContatoServiceTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest
 public class ContatoServiceTest {
     @Autowired
@@ -25,5 +28,23 @@ public class ContatoServiceTest {
         Assertions.assertEquals(contato, objetoDeTeste);
     }
 
+    @Test
+    public void testarMetodoExibirTodosOsContatosCaminhoPositivo(){
+        Contato contato = new Contato();
+        Iterable<Contato> contatoIterable = Arrays.asList(contato);
+        Mockito.when(contatoRepository.findAll()).thenReturn(contatoIterable);
+
+        Assertions.assertTrue(contatoService.exibirTodosOsContatos() instanceof List);
+    }
+
+    @Test
+    public void testarMetodoExibirTodosOsContatosCaminhoPositivoBuscaPeloNome(){
+        Contato contato = new Contato();
+        List<Contato> contatoList= Arrays.asList(contato);
+        Mockito.when(contatoRepository.findByNomeStartsWithIgnoreCase(Mockito.anyString()))
+                .thenReturn(contatoList);
+
+        Assertions.assertEquals(contatoList, contatoService.exibirTodosOsContatos("qualquercoisa"));
+    }
 
 }

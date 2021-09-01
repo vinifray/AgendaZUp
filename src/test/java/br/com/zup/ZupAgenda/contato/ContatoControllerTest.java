bd96357221a1bae639f16370fbb91b35e7d2d46e
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-01 16:02:07 -0300

    Criar metodo de teste caminho positivo do cadastro de contato

--- src/test/java/br/com/zup/ZupAgenda/contato/ContatoControllerTest.java
+++ src/test/java/br/com/zup/ZupAgenda/contato/ContatoControllerTest.java
@@ -1,11 +1,20 @@
 package br.com.zup.ZupAgenda.contato;
 
 import br.com.zup.ZupAgenda.contato.dtos.CadastroContatoDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 //WebMvcTest é a anotação para realização de teste de controller, para ele precisa receber como parametro a classe que será testada
 @WebMvcTest(ContatoController.class)
@@ -18,6 +27,7 @@ public class ContatoControllerTest {
     private MockMvc mockMvc;
 
     private CadastroContatoDTO contatoDTO;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp(){
@@ -25,6 +35,24 @@ public class ContatoControllerTest {
         contatoDTO.setEmail("marvin@dontpanic.com");
         contatoDTO.setTelefone("42 99777878");
         contatoDTO.setNome("Marvin");
+
+        objectMapper = new ObjectMapper();
     }
+
+
+    @Test
+    public void testarRotaDeCadastroDeContatoCaminhoPositivo() throws Exception {
+        Mockito.when(contatoService.salvarContato(Mockito.any(Contato.class)))
+                .thenReturn(contatoDTO.converterDTOemContato());
+        String json = objectMapper.writeValueAsString(contatoDTO);
+
+        ResultActions resultadoDaRequisicao = mockMvc
+                .perform(MockMvcRequestBuilders.post("/contatos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)).andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email",
+                        CoreMatchers.equalTo(contatoDTO.getEmail())));
+    }
+
 
 }

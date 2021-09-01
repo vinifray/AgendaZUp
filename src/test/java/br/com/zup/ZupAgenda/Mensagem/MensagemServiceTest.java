d50af3efde28f2c7ff58f51f2be6dabc1ca3edd3
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-01 13:44:08 -0300

    refactor Mensagem Service Test

--- src/test/java/br/com/zup/ZupAgenda/Mensagem/MensagemServiceTest.java
+++ src/test/java/br/com/zup/ZupAgenda/Mensagem/MensagemServiceTest.java
@@ -1,9 +1,12 @@
-package br.com.zup.ZupAgenda.contato;
+package br.com.zup.ZupAgenda.Mensagem;
 
+import br.com.zup.ZupAgenda.contato.Contato;
+import br.com.zup.ZupAgenda.contato.ContatoService;
 import br.com.zup.ZupAgenda.mensagem.Mensagem;
 import br.com.zup.ZupAgenda.mensagem.MensagemRepository;
 import br.com.zup.ZupAgenda.mensagem.MensagemService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
+import java.util.PrimitiveIterator;
 
 @SpringBootTest
 public class MensagemServiceTest {
@@ -22,11 +26,18 @@ public class MensagemServiceTest {
     @MockBean
     private MensagemRepository mensagemRepository;
 
+    private Contato contato;
+    private Mensagem mensagem;
+
+    @BeforeEach
+    public void setUp(){
+        contato = new Contato();
+        mensagem = new Mensagem();
+        mensagem.setMensagem("Adeus e obrigo pelos peixes");
+    }
+
     @Test
     public void testarCadastroDeMesagemCaminhoPositivo(){
-        Contato contato = new Contato();
-        Mensagem mensagem = new Mensagem();
-        mensagem.setMensagem("Adeus e obrigo pelos peixes");
         Mockito.when(contatoService.buscarContatoPeloId(Mockito.anyInt())).thenReturn(contato);
         Mockito.when(mensagemRepository.save(Mockito.any(Mensagem.class))).thenReturn(mensagem);
 

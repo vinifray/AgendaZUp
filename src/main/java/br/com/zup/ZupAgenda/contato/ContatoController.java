900addd3d8c063a3b888272b01dbaecae7d08593
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-01 15:28:23 -0300

    Refactor Classe controller de contato

--- src/main/java/br/com/zup/ZupAgenda/contato/ContatoController.java
+++ src/main/java/br/com/zup/ZupAgenda/contato/ContatoController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contatos")
-public class Controller {
+public class ContatoController {
     @Autowired
     private ContatoService service;
 

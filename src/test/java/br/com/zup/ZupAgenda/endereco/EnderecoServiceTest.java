afc4e9df59ff0695937c282c98713abbb4f2ef41
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-01 13:52:53 -0300

    Criar metodo de teste para cadastro de endereço

--- src/test/java/br/com/zup/ZupAgenda/endereco/EnderecoServiceTest.java
+++ src/test/java/br/com/zup/ZupAgenda/endereco/EnderecoServiceTest.java
@@ -2,7 +2,10 @@ package br.com.zup.ZupAgenda.endereco;
 
 import br.com.zup.ZupAgenda.contato.Contato;
 import br.com.zup.ZupAgenda.contato.ContatoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -24,5 +27,19 @@ public class EnderecoServiceTest {
     public void setUp(){
         endereco = new Endereco();
         contato = new Contato();
+    }
+
+    @Test
+    public void testarCadastroDeEnderecoCaminhoPositivo(){
+        Mockito.when(contatoService.contatoExistente(Mockito.anyInt())).thenReturn(true);
+        Mockito.when(contatoService.buscarContatoPeloId(Mockito.anyInt())).thenReturn(contato);
+        Mockito.when(contatoService.salvarContato(Mockito.any(Contato.class))).thenReturn(contato);
+        Mockito.when(enderecoRepository.save(Mockito.any(Endereco.class))).thenReturn(endereco);
+
+        Endereco objetoDeTeste = enderecoService.cadastrarEndereço(12, endereco);
+
+        Assertions.assertEquals(contato.getEndereco(), objetoDeTeste);
+        // Mockito verify testa quantas vezes o metodo foi chamado
+        Mockito.verify(contatoService, Mockito.times(1)).salvarContato(contato);
     }
 }
